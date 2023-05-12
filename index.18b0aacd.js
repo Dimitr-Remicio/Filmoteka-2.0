b4047f065ca02dc122930fd52ace85ddb8c56c90
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-05-12 03:46:29 +0000

    Deploying to gh-pages from @ Dimitr-Remicio/Filmoteka1@5f8d9bf6ffcd301bfe6e20f72ec2d5d3b7f00e6d 🚀

--- index.18b0aacd.js
+++
@@ -1,2 +0,0 @@
-var loader=document.querySelector(".loader_page"),timerId=null;function hidenItem(){console.log("hello"),timerId=setInterval((function(){loader.classList.add("loadhide")}),2400)}hidenItem();
-//# sourceMappingURL=index.18b0aacd.js.map
