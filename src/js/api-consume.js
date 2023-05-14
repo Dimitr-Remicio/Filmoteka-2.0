b9e3f5ab874dec3e312edb9967544592d055d6f8
EdiRestrepo <edisonestival@gmail.com>
2023-05-13 21:12:47 -0500

    se muestran las primeras 20 peliculas en el body

--- src/js/api-consume.js
+++ src/js/api-consume.js
@@ -1,6 +1,12 @@
-const apiKey = '2e9f8fc9479fa19131d9c8fc8ea7c110';
-const apiUrl = 'https://api.themoviedb.org/3/movie/2';
-//const apiUrl = 'https://api.themoviedb.org/3';
+import axios from 'axios';
+
+export {services};
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '2e9f8fc9479fa19131d9c8fc8ea7c110';
+// const apiKey = '2e9f8fc9479fa19131d9c8fc8ea7c110';
+// const apiUrl = 'https://api.themoviedb.org/3/movie/2';
+// const apiUrl = 'https://api.themoviedb.org/3';
 
 const categories = {
     trending: '/trending/movie/week',
@@ -10,38 +16,61 @@ const categories = {
   };
 
 // construimos la URL completa con la clave de API
-const url = `${apiUrl}?api_key=${apiKey}`;
-//const url = `${apiUrl}${categories.trending}?api_key=${apiKey}${categories.basic}&page=1`;
+// const url = `${BASE_URL}?api_key=${API_KEY}`;
+const url = `${BASE_URL}${categories.trending}?api_key=${API_KEY}${categories.basic}&page=1`;
+
+
+//proxi
+const services = {
+  getMovies: async function(searchTerm, page,RESULTS_PER_PAGE){
+    try {
+      // console.log('hola desde inicio axios')
+      const response = await axios.get(url);
+      const movies = response;
+      // console.log('hola desde respuesta axios')
+      console.log(movies.data.results)
+      console.log('hola desde fin axios')
+      return movies.data.results;
+    } catch (error) {
+      console.log('hola desde error')
+      console.error(error);
+    }
+  }
+}
+
 
 // URL base para las imágenes de la API
 const baseImageUrl = 'https://image.tmdb.org/t/p/';
+// const urlPoster = `${baseImageUrl}w500${data.poster_path}`;
+
 
 // Seleccionamos el elemento HTML donde se mostrará el título
-const tituloPelicula = document.getElementById('titulo-pelicula');
-const overview = document.getElementById('descripcion-pelicula');
-const lanzamiento = document.getElementById('ano-lanzamiento');
-const posterPelicula = document.getElementById('poster-pelicula');
+// const tituloPelicula = document.getElementById('titulo-pelicula');
+// const overview = document.getElementById('descripcion-pelicula');
+// const lanzamiento = document.getElementById('ano-lanzamiento');
+// const posterPelicula = document.getElementById('poster-pelicula');
 
 // Hacer la solicitud GET a la API
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
+// export const data = fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
     // mostramos los datos de respuesta en consola
-    console.log(data);
+    // console.log(data);
 
     // trabajamos los datos
-    const titulo = data.title;
-    const resumen = data.overview;
-    const relase = data.release_date;
-    const urlPoster = `${baseImageUrl}w500${data.poster_path}`;
+    // const titulo = data.title;
+    // const resumen = data.overview;
+    // const relase = data.release_date;
+    
     
     // Actualizar el contenido del elemento HTML con el título de la película
-    tituloPelicula.textContent = titulo;
-    overview.textContent = resumen;
-    lanzamiento.textContent = relase;
-    posterPelicula.src = urlPoster;
-  })
-  .catch(error => {
-    // Manejo de errores
-    console.log('Ha ocurrido un error:', error);
-  });
+    // tituloPelicula.textContent = titulo;
+    // overview.textContent = resumen;
+    // lanzamiento.textContent = relase;
+    // posterPelicula.src = urlPoster;
+  //   return data;
+  // })
+  // .catch(error => {
+  //   // Manejo de errores
+  //   console.log('Ha ocurrido un error:', error);
+  // });
