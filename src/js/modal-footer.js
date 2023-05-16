80cc0b43fdb0e4d160eda73734ee94240108adaf
Ana <ana.pereirar@utadeo.edu.co>
2023-05-15 22:16:37 -0500

    Escape event

--- src/js/modal-footer.js
+++ src/js/modal-footer.js
@@ -1,16 +1,28 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })
-  
-  ();
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  document.addEventListener("keydown", handleKeyPress);
+  document.addEventListener("click", handleOutsideClick);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === "Escape") {
+      toggleModal();
+    }
+  }
+
+  function handleOutsideClick(event) {
+    if (!event.target.closest("[data-modal]")) {
+      toggleModal();
+    }
+  }
+})();
