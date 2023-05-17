e9a48a0950c1b0c3cc377df57e115d89ab56e2cf
EdiRestrepo <edisonestival@gmail.com>
2023-05-16 20:50:36 -0500

    Se renderiza tarjeta con peliculas de tendencia

--- src/components/body.js
+++ src/components/body.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import { services } from '../js/api-consume';
+import { getName, getSome } from '../js/genres';
 import './bodyStyles.scss';
 
 let markup = '';
@@ -8,19 +9,21 @@ const cardsMovies = document.querySelector('.cardsMovie');
 const baseImageUrl = 'https://image.tmdb.org/t/p/';
 
 const layoutUtils = {
-  getMoviesIds: function (movies){
-    const movieIds = [];
-    movies.map(({id})=>{
-      movieIds.push(id)
-    })
-    return movieIds;
-  },
+  // getMoviesIds: function (movies){
+  //   const movieIds = [];
+  //   movies.map(({id})=>{
+  //     movieIds.push(id)
+  //   })
+  //   return movieIds;
+  // },
   renderImages: function (movies) {
     markup += movies
-      .map(({ backdrop_path }) => {
+      .map(({ backdrop_path, original_title, release_date, genre_ids}) => {
         return `<li>
             <img class='cardsMovie__image' src='${baseImageUrl}w500${backdrop_path
             }' alt='image movie'/>
+            <p>${original_title}</p>
+            <p>${getSome(genre_ids)}${release_date}</p>
             </li>`;
       })
       .join('');
@@ -28,12 +31,12 @@ const layoutUtils = {
   },
   refreshMovieList: async function () {
     const movies = await services.getMovies();
-    console.log(movies);
-    const arrayMovies =  this.getMoviesIds(movies)
-    console.log(arrayMovies)
-    const arrayMoviesList = await services.getArrayMovies(arrayMovies);
-    console.log(arrayMoviesList);
-    return this.renderImages(arrayMoviesList);
+    // console.log(movies);
+    // const arrayMovies =  this.getMoviesIds(movies)
+    // console.log(arrayMovies)
+    // const arrayMoviesList = await services.getArrayMovies(arrayMovies);
+    // console.log(arrayMoviesList);
+    return this.renderImages(movies);
   },
 };
 
